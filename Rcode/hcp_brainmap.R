7a41312c2fd51023fce934632b6b19663252f28a
Zeyi <zwang107@gmail.com>
2018-02-01 23:52:38 -0500

    better interactive plot

--- Rcode/hcp_brainmap.R
+++ Rcode/hcp_brainmap.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(rgl)
 library(misc3d)
+library(oro.nifti)
 
 record_pair <- readRDS("./data/record_pair.rds")
 
@@ -9,7 +10,9 @@ pair_select <-  (p.adjust(1-ppois(record_pair - 1, 1), method = "BH") < 0.05) * 
 record_line <- readRDS("./data/record_line.rds")
 templateImage <- readRDS("./data/templateImage.rds")
 
-contour3d(templateImage, level= 1, smooth = 20, fill = TRUE, mask = array(TRUE, dim(templateImage)), alpha = .2, add = FALSE)
+contour3d(templateImage, level= 1, smooth = 20, fill = TRUE, mask = array(TRUE, dim(templateImage)), alpha = .1, add = FALSE)
 segments3d(record_line[,1], record_line[,2], record_line[,3], lwd = 1)
 points3d(unique(record_line)[,1],unique(record_line)[,2], unique(record_line)[,3], 
          color = "blue", size = 10)
+
+# install.packages(c("rgl", "rglwidget"), repos="http://R-Forge.R-project.org")
